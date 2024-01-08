d24ab58b501df25604b6a55f6a78e5c74fff4abe
Ing. Rosa Severiche <114374804+ingseveriche@users.noreply.github.com>
2024-01-08 16:50:26 -0500

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+//use App\Models\Image;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//GENERALES
+Auth::routes();
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//USUARIO
+Route::get('/configuracion', [App\Http\Controllers\UserController::class, 'config'])->name('config');
+Route::post('/user/update', [App\Http\Controllers\UserController::class, 'update'])->name('user.update');
+Route::get('/user/avatar/{filename}', [App\Http\Controllers\UserController::class, 'getImage'])->name('user.avatar');
+Route::get('/perfil/{id}', [App\Http\Controllers\UserController::class, 'profile'])->name('profile');
+Route::get('/gente/{search?}', [App\Http\Controllers\UserController::class, 'index'])->name('user.index');
+
+//IMAGEN
+Route::get('/subir-imagen', [App\Http\Controllers\ImageController::class, 'create'])->name('image.create');
+Route::post('/image/save', [App\Http\Controllers\ImageController::class, 'save'])->name('image.save');
+Route::get('/image/file/{filename}', [App\Http\Controllers\ImageController::class, 'getImage'])->name('image.file');
+Route::get('/image/{id}', [App\Http\Controllers\ImageController::class, 'detail'])->name('image.detail');
+Route::get('/image/delete/{id}', [App\Http\Controllers\ImageController::class, 'delete'])->name('image.delete');
+Route::get('/imagen/editar/{id}', [App\Http\Controllers\ImageController::class, 'edit'])->name('image.edit');
+Route::post('/image/update', [App\Http\Controllers\ImageController::class, 'update'])->name('image.update');
+
+//COMENTARIO
+Route::post('/comment/save', [App\Http\Controllers\CommentController::class, 'save'])->name('comment.save');
+Route::get('/comment/delete/{id}', [App\Http\Controllers\CommentController::class, 'delete'])->name('comment.delete');
+
+//LIKE
+Route::get('/like/{image_id}', [App\Http\Controllers\LikeController::class, 'like'])->name('like.save');
+Route::get('/dislike/{image_id}', [App\Http\Controllers\LikeController::class, 'dislike'])->name('like.delete');
+Route::get('/likes', [App\Http\Controllers\LikeController::class, 'index'])->name('likes');
